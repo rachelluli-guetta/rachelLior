00885d5b586798abe2c4382d160191dab95a67ab
RACHELG <rachelg@il.ibm.com>
2017-05-24 14:05:41 +0300

    get all users first implementation

--- index.js
+++ index.js
@@ -5,10 +5,35 @@
 const AWS = require('aws-sdk');
 const express = require('express');
 const app = express();
+const s3 = new AWS.S3();
 
 //User is stored by:
 //key: Base64(username_password)
 //user data: {firstName: ..., lastName: ..., email: ..., phoneNo: ...}
+
+const bucketName = 'rachel-lior-users';
+const getAllUsersParams = {
+    Bucket: bucketName,
+    Prefix: 'users'
+}
+
+
+app.get('/getAllUsers', function(req, res){
+
+    console.log('Getting all the users..');
+
+    s3.listObjects(getAllUsersParams, function (err, data) {
+        if(err) {
+            console.log('Error when listing the bucket objects: ' + JSON.stringify(err));
+        }
+
+        var contents = data.Contents;
+
+        res.send(JSON.stringify(contents))
+        console.log('Found data: ' + JSON.stringify(data));
+    });
+
+})
 
 
 app.listen(3000, function() {
