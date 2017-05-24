1529bfd46e84a926c8893b687dd90c8d441683f4
RACHELG <rachelg@il.ibm.com>
2017-05-24 11:39:37 +0300

    express app listens on port 3000

--- index.js
+++ index.js
@@ -2,5 +2,12 @@
  * Created by rachelg on 24/05/2017.
  */
 
+const AWS = require('aws-sdk');
+const express = require('express');
+const app = express();
 
-console.log("hi lior")
+
+
+app.listen(3000, function() {
+    console.log('Listening on port 3000..');
+})
