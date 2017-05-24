5a83042e718ecb3031b090e67852c9aaa23018f7
Lior Burg <liorbu@il.ibm.com>
2017-05-24 14:06:44 +0300

    Added postUser endpoint

--- index.js
+++ index.js
@@ -6,8 +6,36 @@ const AWS = require('aws-sdk');
 const express = require('express');
 const app = express();
 
+const s3 = new AWS.S3();
 
+const bucketname =
 
 app.listen(3000, function() {
     console.log('Listening on port 3000..');
+
+    // s3.listBuckets(function(error, data) {
+    //     if (error) {
+    //         console.log(error); // error is Response.error
+    //     } else {
+    //         console.log(data); // data is Response.data
+    //     }
+    // });
+
+    params = {Bucket: myBucket, Key: myKey, Body: 'Hello!'};
+
+    s3.putObject(params, function(err, data) {
+
+        if (err) {
+
+            console.log(err)
+
+        } else {
+
+            console.log("Successfully uploaded data to myBucket/myKey");
+
+        }
+
+    });
+
+
 })
